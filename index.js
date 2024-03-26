4a160e80b35934879e7217259ef5081339acf604
B Bhanu Prasanth <115921179+Bhanuprasanth2003@users.noreply.github.com>
2024-03-26 17:30:59 +0530

    index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://bhanuprasanth3:BRMQPClCcSKUpanc@cluster0.9ustcal.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { useNewUrlParser: true , useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://bhanuprasanth3:BRMQPClCcSKUpanc@cluster0.9ustcal.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 const postSchema = new mongoose.Schema( {
                                          title: String,
